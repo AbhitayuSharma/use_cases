6dd28f656c6abdb9749d29bc5df81cc448039a1d
AbhitayuSharma <abhitayu.sharma@kpipartners.com>
2022-10-27 20:15:49 +0530

    Committing "Currency Conversion"

--- demo/src/main/java/com/kpi/microservice/currencyexchangeexampleservice/demo/CurrencyExchangeSampleController.java
+++ demo/src/main/java/com/kpi/microservice/currencyexchangeexampleservice/demo/CurrencyExchangeSampleController.java
@@ -1,5 +1,21 @@
 package com.kpi.microservice.currencyexchangeexampleservice.demo;
 
-public class CurrencyExchangeSampleController {
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeSampleController {
+	@Autowired
+	private ExchangeValueRepo ev;
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public ExchangeValueEntity convertCurrency(@PathVariable String from,@PathVariable String to)  
+	{  
+		return ev.findByFromAndTo(from, to);
+		
+	}  
+	
 }
